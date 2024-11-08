fbf6f4adf2134e46e91cd622ea51725c438b47d6
Jonas Monteiro <mfernandes.jonas@gmail.com>
2024-11-07 21:02:44 -0300

    feat: started main

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Header from "./components/Header"
+import Main from "./components/Main"
 
 const App = () => {
   return (
     <>
+    <div className="max-w-lg">
     <Header/>
+    <Main />
+    </div>
     </>
   )
 }
---
+++ src/components/Main.tsx
@@ -0,0 +1,24 @@
+const Main = () => {
+  return (
+    <>
+      <section className="p-5 flex flex-col gap-6">
+        <p className="text-tertiary leading-7 text-sm md:text-base md:text-justify">
+          I'm a software engineer with over 2 years of experience specializing
+          in accessible, pixel-perfect interfaces. I combine design and
+          development to create seamless user experiences. I refine my skills
+          through side projects and engage a 75k+ community of aspiring
+          developers, bridging frontend, UX, and design.
+        </p>
+        
+        <p className="text-tertiary leading-7 text-sm md:text-base md:text-justify">
+          When I'm not coding, you'll find me drawing digital art and watching
+          anime – which actually inspires a lot of my UI design aesthetics. I
+          believe that bringing together my passion for art and technology helps
+          me create more engaging and visually appealing web experiences.
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default Main;
