baa419433c64d29509d1b9b43dfa5308378893e0
Jonas Monteiro <mfernandes.jonas@gmail.com>
2024-11-08 19:44:16 -0300

    fix: fixed imports on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,9 @@
 import { HEADER } from "../constants";
-import { HEADER_PT } from "../constants";
 import Logo from "../assets/pfp.png";
 import { Languages } from "lucide-react";
-import { useState } from "react";
-import { useLanguage } from "./LanguageContext";
 
 const Header = () => {
   
-    const {toggleLanguage, language} = useLanguage()
   return (
     <>
       <header className="flex items-center space-x-3 mb-10 w-full">
@@ -21,7 +17,6 @@ const Header = () => {
           <h2 className="text-secondary text-sm">{HEADER.role}</h2>
         </div>
         <button
-          onClick={toggleLanguage}
           className="ml-auto text-blue cursor-pointer"
         >
           <Languages />
